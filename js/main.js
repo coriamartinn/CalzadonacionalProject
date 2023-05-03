07985a6e2b93e849b058ba88b7602554520fd8a8
Martin Coria <coria10m@gmail.com>
2023-05-02 21:10:26 -0300

    todo lo nuevo

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ const nav    = document.querySelector('.nav')
 button.addEventListener('click',()=>{
     nav.classList.toggle('active')
 })
+
+
+// texto main
+
+var typed = new Typed('.typed', {
+    strings: ['calzado nacional', 'El calzado que vos <br>necesitas...'],
+    //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+  });
